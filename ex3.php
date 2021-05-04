0e9d2b54561e1f246f349f3c68b9dda68bfcb10e
Nicolas Vauché <hello@nicolasvauche.net>
2021-05-04 13:13:09 +0200

    Insertion de données - 1/3

--- ex3.php
+++ ex3.php
@@ -5,12 +5,23 @@ $dbname = 'php_test';
 $user = 'root';
 $pass = 'root';
 
+// Initialisation des données exemple
+$userData = [
+    'prenom' => 'Bob',
+    'nom' => 'Marley',
+];
+
 // On sécurise une opération qui pourrait très probablement créer une exception
 try {
     // Allez, on tente la connection à la BDD, avec l'aide de PDO
     $dbco = new PDO("mysql:host=$servname;dbname=$dbname", $user, $pass);
     $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "<p>Connexion à la BDD OK :)</p>";
+
+    // Bon, on va tenter d'ajouter des données dans la table user. Mais salement…
+    $sql = "INSERT INTO user(prenom, nom) VALUES('{$userData['prenom']}', '{$userData['nom']}');";
+    $dbco->exec($sql);
+    echo "<p>{$userData['prenom']} {$userData['nom']} a été ajouté</p>";
 } catch (PDOException $e) {
     // L'exception levée par PDO est correctement captée ici
     echo "<p>Une Exception a été levée :(<br />" . $e->getMessage() . "</p>";
