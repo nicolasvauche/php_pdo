01aa5fb3c04d7f57ae6b5d332645a58ce07172a3
Nicolas Vauché <hello@nicolasvauche.net>
2021-05-21 13:09:47 +0200

    Fix BDD connexion Exceptions

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+/* ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL); */
+
 // Initialisation des variables de configuration
 require_once('utils/json.php');
 if (!$dbData = fileJsonToArray('config/bdd.json')) {
@@ -11,13 +15,19 @@ $userData = [
     'nom' => 'Marley',
 ];
 
-// On sécurise une opération qui pourrait très probablement créer une exception
+// On sécurise la connexion à la base de données
 try {
     // Allez, on tente la connection à la BDD, avec l'aide de PDO
     $dbco = new PDO("mysql:host={$dbData['servname']};dbname={$dbData['dbname']}", $dbData['user'], $dbData['pass']);
     $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "<p>Connexion à la BDD OK :)</p>";
+} catch (PDOException $e) {
+    // L'exception levée par PDO est correctement captée ici
+    echo "<p>Erreur de connexion à la base de données :(<br />" . $e->getMessage() . "</p>";
+}
 
+// On sécurise une opération qui pourrait très probablement créer une exception
+try {
     // Bon, on va tenter d'ajouter des données dans la table user. Proprement cette fois !
     $dbco->beginTransaction();
     $req = $dbco->prepare("INSERT INTO user(prenom, nom) VALUES(:prenom, :nom);");
